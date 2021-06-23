6681697485dade5475b9ac055e484f310e4e3b60
Harmandeep Singh <deepharman2000@gmail.com>
2021-06-22 20:48:09 -0400

    barebone backend frontend typescript

---
+++ src/client/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  constructor(props: any) {
+    super(props)
+  }
+  render(){
+    return(
+      <h1>driver info</h1>
+    )
+  }
+}
+
+export default App;
